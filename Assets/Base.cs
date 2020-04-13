e1b08976a1b61632cbf9acfa0ae6732378e43acd
CMorar143 <cian@morar.org>
2020-04-13 23:17:41 +0100

    added base colour

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -5,20 +5,42 @@ using UnityEngine;
 
 public class Base : MonoBehaviour
 {
-    public float tiberium = 0;
-
+	public float tiberium = 0;
     public TextMeshPro text;
-
     public GameObject fighterPrefab;
 
+	private IEnumerator AccumulateTiberium(float time)
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(time);
+			tiberium += 1;
 
-    // Start is called before the first frame update
-    void Start()
+			if (tiberium > 10)
+			{
+				CreateShip();
+			}
+		}
+	}
+
+	private void CreateShip()
+	{
+		tiberium -= 10;
+		GameObject ship = Instantiate(fighterPrefab, transform.position + new Vector3(0, 0, 2), Quaternion.identity);
+		ship.transform.parent = this.transform;
+		ship.GetComponent<Renderer>().material.color = gameObject.GetComponent<Renderer>().material.color;
+		ship.GetComponentInChildren<MeshRenderer>().material.color = gameObject.GetComponent<Renderer>().material.color;
+	}
+
+	private void OnEnable()
+	{
+		StartCoroutine(AccumulateTiberium(1.0f));
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-        foreach (Renderer r in GetComponentsInChildren<Renderer>())
-        {
-            r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
-        }
+
     }
 
     // Update is called once per frame
